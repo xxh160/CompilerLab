2899970b12af2e5cb918cb13af5355cab766d934
tracer-compiler2021 <compilers@software.nju.edu.cb>
2021-11-18 22:39:24 +0800

    >  make 191250045 xiayi Linux xiayi 5.15.2-arch1-1 #1 SMP PREEMPT Fri, 12 Nov 2021 19:22:10 +0000 x86_64 GNU/Linux  22:39:24 up  6:18,  3 users,  load average: 2.50, 1.75, 1.29 1d3079277adc19587edd954f9fee64c4751666d

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,8 @@ public class Main {
         var tokenList = lexer.getAllTokens();
 //        if (lexer.getFaults() > 0) return;
         for (var i : tokenList) {
-            System.out.println(i.getType());
+            System.out.print(i.getType());
+            System.out.println(" " + i);
         }
     }
 
