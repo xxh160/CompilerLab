04f427cdfcf127761ba41592e42cd621b65f031f
tracer-compiler2021 <compilers@software.nju.edu.cb>
2021-11-18 22:23:51 +0800

    >  make 191250045 xiayi Linux xiayi 5.15.2-arch1-1 #1 SMP PREEMPT Fri, 12 Nov 2021 19:22:10 +0000 x86_64 GNU/Linux  22:23:51 up  6:02,  3 users,  load average: 1.28, 0.97, 1.03 9564af0b9cf7cdc85f0b6ec6110d1da0a47d80f9

--- src/CmmLexerImpl.java
+++ src/CmmLexerImpl.java
@@ -1,9 +1,17 @@
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.LexerNoViableAltException;
+import org.antlr.v4.runtime.misc.Interval;
 
 public class CmmLexerImpl extends CmmLexer {
 
     public CmmLexerImpl(CharStream input) {
         super(input);
     }
-    
+
+    @Override
+    public void notifyListeners(LexerNoViableAltException e) {
+        String text = this._input.getText(Interval.of(this._tokenStartCharIndex, this._input.index()));
+        System.err.printf("Error type A at Line %d: %s.", this._tokenStartLine, this.getErrorDisplay(text));
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
 
 import java.io.FileInputStream;
 
 public class Main {
 
     public static void main(String[] args) {
+        CmmLexerImpl lexer = null;
         try {
             ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(args[0]));
-            CmmLexer cmmLexer = new CmmLexer(input);
+            lexer = new CmmLexerImpl(input);
         } catch (Exception e) {
+            e.printStackTrace();
         }
+        assert lexer != null;
+        var tokenList = lexer.getAllTokens();
+        for (var i : tokenList) System.out.println(i);
     }
 
 }
